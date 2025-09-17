16a404528ab11e82b96ba8436ed65b73d3a9ed0f
Oskar Codes <oskarbertrand@icloud.com>
2025-09-17 19:09:00 +0200

    Add function to send text

--- src/assets/js/file-transfer.js
+++ src/assets/js/file-transfer.js
@@ -2,7 +2,7 @@
 export const connectionPrefix = '7b965b547af4fb240fd3c0de264928b3fda5763fdf70b9344b5cec4d43f0ff89';
 
 export function createPeer() {
-  const id = new Array(6).fill().map(e => Math.floor(Math.random() * 10)).join("").toString();
+  const id = new Array(6).fill().map(_ => Math.floor(Math.random() * 10)).join("").toString();
   const peer = new Peer(connectionPrefix + id);
   peer.originalId = id;
   return peer;
@@ -14,8 +14,21 @@ export function sendFile(otherId, file, success, error) {
   const connection = peerInstance.connect(connectionPrefix + otherId);
   connection.on('open', () => {
     connection.send({
+      type: 'file',
       name: file.name,
       file
+    });
+  });
+  if (success) connection.on('close', success)
+  if (error) connection.on('error', error)
+}
+
+export function sendText(otherId, text, success, error) {
+  const connection = peerInstance.connect(connectionPrefix + otherId);
+  connection.on('open', () => {
+    connection.send({
+      type: 'text',
+      text
     });
   });
   if (success) connection.on('close', success)
